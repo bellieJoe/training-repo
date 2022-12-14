2ea334d1227ff21f627216e1316c839fa4395c8f
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2022-12-14 22:35:51 +0800

    Edit movielibrary console app

--- MovieLibrary/Program.cs
+++ MovieLibrary/Program.cs
@@ -8,8 +8,48 @@ namespace MovieLibrary
 {
     class Program
     {
+
+        public static void displayMenu()
+        {
+            Console.WriteLine("***************************************");
+            Console.WriteLine("*         WELCOME TO MOVIELIB         *");
+            Console.WriteLine("*        Movie Personal Ratings       *");
+            Console.WriteLine("***************************************");
+            Console.WriteLine("\nWhat do you want to do?");
+            Console.WriteLine("ENTER 1: Display all Movies, Ratings and Reviews");
+            Console.WriteLine("ENTER 2: Add new Movie");
+            Console.WriteLine("ENTER 3: Add new Ratings");
+            Console.WriteLine("ENTER 4: Display Top 10 Movies");
+            Console.WriteLine("ENTER 0: Exit Application");
+            Console.Write("\nINPUT: ");
+        }
+
+
+        public static void processInput(int userInput)
+        {
+            Console.WriteLine(userInput);
+        }
+
+
+        public static void startApplication()
+        {
+            int userInput;
+            do
+            {
+                displayMenu();
+                userInput = Convert.ToInt32(Console.ReadLine());
+                processInput(userInput);
+            }
+            while (userInput != 0);
+        }
+
+
+        
+
         static void Main(string[] args)
         {
+            startApplication();
+            Console.ReadLine();
         }
     }
 }
