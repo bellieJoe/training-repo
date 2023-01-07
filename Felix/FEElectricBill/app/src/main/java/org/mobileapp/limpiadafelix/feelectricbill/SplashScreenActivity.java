88e4dab720ecd10ff0bc46a9f092f6a8f9c3e3e3
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2023-01-07 23:32:10 +0800

    Added Final Exams

---
+++ Felix/FEElectricBill/app/src/main/java/org/mobileapp/limpiadafelix/feelectricbill/SplashScreenActivity.java
@@ -0,0 +1,39 @@
+package org.mobileapp.limpiadafelix.feelectricbill;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowInsets;
+
+import org.mobileapp.limpiadafelix.feelectricbill.databinding.ActivitySplashScreenBinding;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity  .this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+    }
+
+}
